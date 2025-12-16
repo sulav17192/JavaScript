3d1c6dd1d992ea0ef0941b814f5e6ff41e86df21
Sulav Aryal <aryalpurna6@gmail.com>
2025-12-16 21:30:23 +0545

    Timer functions in JS

---
+++ Day-2/timer.js
@@ -0,0 +1,14 @@
+
+// setTimeout function
+function myfunction(){
+    console.log("Hello world!!");
+}
+setTimeout(myfunction, 1000);
+
+
+//setinterval function
+
+function nextfunction(){
+    console.log("Exectting in every 2 seconds");
+}
+setInterval(nextfunction, 2000);
