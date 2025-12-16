60e42c18e46cd0cf0a3972c1f620d0b09f0d0e2e
Sulav Aryal <aryalpurna6@gmail.com>
2025-12-16 21:39:13 +0545

    Synchronous Approach

---
+++ Day-2/synchronous.js
@@ -0,0 +1,10 @@
+// synchronous programming
+// It is programming approach in which earlier line of code is exect-uted first then only
+// another line gets turn. it is linear and predictable approach of programming. 
+
+//Example
+
+
+console.log("Hello World!");
+console.log("Learning JS");
+console.log("Learning Synchronous approach of programming");
